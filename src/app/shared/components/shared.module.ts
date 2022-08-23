2a1a1159827afc1f13addf9004ec7fe0b0ca38f2
Gvrito <gvritishvili.gio123@gmail.com>
2022-08-23 20:27:12 +0400

    add file upload component and shared module

---
+++ src/app/shared/components/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+
+
+@NgModule({
+    imports: [],
+    exports: [
+        FileUploadComponent
+    ],
+    declarations: [
+        FileUploadComponent
+    ],
+    providers: [],
+})
+export class SharedModule { }
